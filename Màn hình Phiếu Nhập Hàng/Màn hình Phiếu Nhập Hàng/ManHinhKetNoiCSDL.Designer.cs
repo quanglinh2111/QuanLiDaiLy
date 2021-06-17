a5ae8a03cfbf2b0fe1d1401eeeac995a61e2eaa2
DenisTu21 <82129252+DenisTu21@users.noreply.github.com>
2021-06-17 17:26:23 +0700

    commit màn hình kết nối CSDL
    màn hình kết nối CSDL

---
+++ Màn hình Phiếu Nhập Hàng/Màn hình Phiếu Nhập Hàng/ManHinhKetNoiCSDL.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace Màn_hình_Phiếu_Nhập_Hàng
+{
+    partial class ManHinhKetNoiCSDL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManHinhKetNoiCSDL));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbBoxSerVerName = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbBoxAuthentication = new System.Windows.Forms.ComboBox();
+            this.txtBPassWord = new System.Windows.Forms.TextBox();
+            this.txtBUserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbBoxDatabase = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnTestConnection = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnKetNoi = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(-1, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(613, 654);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(673, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SERVER NAME:";
+            // 
+            // cbBoxSerVerName
+            // 
+            this.cbBoxSerVerName.DropDownHeight = 150;
+            this.cbBoxSerVerName.FormattingEnabled = true;
+            this.cbBoxSerVerName.IntegralHeight = false;
+            this.cbBoxSerVerName.Location = new System.Drawing.Point(911, 163);
+            this.cbBoxSerVerName.Name = "cbBoxSerVerName";
+            this.cbBoxSerVerName.Size = new System.Drawing.Size(353, 28);
+            this.cbBoxSerVerName.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(671, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AUTHENTICATION:";
+            // 
+            // cbBoxAuthentication
+            // 
+            this.cbBoxAuthentication.DropDownHeight = 150;
+            this.cbBoxAuthentication.FormattingEnabled = true;
+            this.cbBoxAuthentication.IntegralHeight = false;
+            this.cbBoxAuthentication.Location = new System.Drawing.Point(912, 236);
+            this.cbBoxAuthentication.Name = "cbBoxAuthentication";
+            this.cbBoxAuthentication.Size = new System.Drawing.Size(353, 28);
+            this.cbBoxAuthentication.TabIndex = 5;
+            // 
+            // txtBPassWord
+            // 
+            this.txtBPassWord.Location = new System.Drawing.Point(911, 380);
+            this.txtBPassWord.Multiline = true;
+            this.txtBPassWord.Name = "txtBPassWord";
+            this.txtBPassWord.Size = new System.Drawing.Size(353, 39);
+            this.txtBPassWord.TabIndex = 12;
+            // 
+            // txtBUserName
+            // 
+            this.txtBUserName.Location = new System.Drawing.Point(912, 310);
+            this.txtBUserName.Multiline = true;
+            this.txtBUserName.Name = "txtBUserName";
+            this.txtBUserName.Size = new System.Drawing.Size(353, 37);
+            this.txtBUserName.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(671, 314);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 29);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "USER NAME:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(673, 383);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 29);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "PASS WORD:";
+            // 
+            // cbBoxDatabase
+            // 
+            this.cbBoxDatabase.DropDownHeight = 150;
+            this.cbBoxDatabase.FormattingEnabled = true;
+            this.cbBoxDatabase.IntegralHeight = false;
+            this.cbBoxDatabase.Location = new System.Drawing.Point(912, 462);
+            this.cbBoxDatabase.Name = "cbBoxDatabase";
+            this.cbBoxDatabase.Size = new System.Drawing.Size(353, 28);
+            this.cbBoxDatabase.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(671, 462);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 29);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "DATABASE:";
+            // 
+            // btnTestConnection
+            // 
+            this.btnTestConnection.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnTestConnection.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTestConnection.Location = new System.Drawing.Point(911, 533);
+            this.btnTestConnection.Name = "btnTestConnection";
+            this.btnTestConnection.Size = new System.Drawing.Size(354, 36);
+            this.btnTestConnection.TabIndex = 17;
+            this.btnTestConnection.Text = "Test Connection";
+            this.btnTestConnection.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.Location = new System.Drawing.Point(629, 585);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(634, 10);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Location = new System.Drawing.Point(618, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(17, 654);
+            this.button2.TabIndex = 19;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // btnKetNoi
+            // 
+            this.btnKetNoi.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnKetNoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKetNoi.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnKetNoi.Image = ((System.Drawing.Image)(resources.GetObject("btnKetNoi.Image")));
+            this.btnKetNoi.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnKetNoi.Location = new System.Drawing.Point(1125, 627);
+            this.btnKetNoi.Name = "btnKetNoi";
+            this.btnKetNoi.Size = new System.Drawing.Size(137, 54);
+            this.btnKetNoi.TabIndex = 20;
+            this.btnKetNoi.Text = "Kết nối";
+            this.btnKetNoi.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnKetNoi.UseVisualStyleBackColor = false;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
+            this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThoat.Location = new System.Drawing.Point(912, 627);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(116, 54);
+            this.btnThoat.TabIndex = 21;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThoat.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(708, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(530, 64);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Kết nối cơ sở dữ liệu";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button4.Location = new System.Drawing.Point(631, 109);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(634, 10);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // ManHinhKetNoiCSDL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1334, 733);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnKetNoi);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnTestConnection);
+            this.Controls.Add(this.cbBoxDatabase);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBPassWord);
+            this.Controls.Add(this.txtBUserName);
+            this.Controls.Add(this.cbBoxAuthentication);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbBoxSerVerName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "ManHinhKetNoiCSDL";
+            this.Text = "Màn hình kết nối CSDL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbBoxSerVerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbBoxAuthentication;
+        private System.Windows.Forms.TextBox txtBPassWord;
+        private System.Windows.Forms.TextBox txtBUserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbBoxDatabase;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnTestConnection;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnKetNoi;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+    }
+}
